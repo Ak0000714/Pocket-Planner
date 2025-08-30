1fbb952c303815680100a1f6b8112f6efce42f5b
AJMAL KHAN.M <126975933+AjmalKhan203@users.noreply.github.com>
2025-08-31 01:09:18 +0530

    connect_db.py

--- pocket planner/connect_db.py
+++ pocket planner/connect_db.py
@@ -4,6 +4,7 @@ def connect_db():
     return mysql.connector.connect(
         host="localhost",  # MySQL host
         user="root",       # Your MySQL username
-        password="ajmal203",   # Your MySQL password
+        password="###",   # Your MySQL password
         database="expense_tracker"  # The name of your database
     )
+
